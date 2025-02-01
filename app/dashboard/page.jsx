36cbb6a3dcc02ba67fa96798b92df01530951ade
Shivam Pande <shivshivampande18@gmail.com>
2025-02-01 22:48:08 +0530

    Added suspension in dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import {
     BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer,
     Cell, PieChart, Pie
@@ -464,4 +464,10 @@ const DashboardPage = () => {
     );
 };
 
-export default DashboardPage;
+export default function PageWrapper() {
+    return (
+        <Suspense fallback={<p>Loading...</p>}>
+            <DashboardPage />
+        </Suspense>
+    );
+}
